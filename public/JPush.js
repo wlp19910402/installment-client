7bce26c398a1c1dc2e6f7a22c2b0bcce65c9527f
王莉萍 <5556929+wlp6897@user.noreply.gitee.com>
2020-12-01 17:19:22 +0800

    添加极光推送

--- public/JPush.js
+++ public/JPush.js
@@ -1,30 +1,34 @@
-
+/**
+ * 极光推送收到通知和打开通知的相应动作
+ */
 document.addEventListener(
   "jpush.openNotification",
-  function () {
+  function (event) {
     //打开通知
     try {
       //获取极光推送附带的参数
       var id = event.extras.id;
       var lx = event.extras.lx;
+      alert(JSON.stringify(event.extras));
       //相应的动作
     } catch (exception) {
-      console.log("JPushPlugin:onOpenNotification" + exception);
+      alert("JPushPlugin:onOpenNotification" + exception);
     }
   },
   false
 );
 document.addEventListener(
   "jpush.receiveNotification",
-  function () {
+  function (event) {
     //收到通知
     try {
       //获取极光推送附带的参数
       var id = event.extras.id;
       var lx = event.extras.lx;
+      alert(JSON.stringify(event.extras));
       //相应的动作
     } catch (exception) {
-      console.log("JPushPlugin:onReceiveNotification" + exception);
+      alert("JPushPlugin:onReceiveNotification" + exception);
     }
   },
   false
--- src/index.js
+++ src/index.js
@@ -18,4 +18,49 @@ ReactDOM.render(
  * unregister() to register() below. Note this comes with some pitfalls.
  * Learn more about service workers: https://bit.ly/CRA-PWA
  */
+
+var app = {
+  initialize: function () {
+    alert("===[注册初始化事件]===");
+    document.addEventListener("deviceready", this.onDeviceReady.bind(this), false);
+  },
+  onDeviceReady: function () {
+    //设备准备完毕
+    alert("===[设备准备就绪]===");
+    initJPush(); //初始化极光推送
+  },
+};
+app.initialize();
+
+function initJPush() {
+  alert("===[初始化极光推送]===");
+  try {
+    window.JPush.init();
+    window.JPush.setDebugMode(true);
+    if (window.device.platform !== "Android") {
+      window.JPush.setApplicationIconBadgeNumber(0);
+    }
+  } catch (exception) {
+    alert(exception);
+  }
+  document.addEventListener(
+    "jpush.receiveRegistrationId",
+    function (event) {
+      alert("receiveRegistrationId" + JSON.stringify(event));
+    },
+    false
+  );
+}
+// function setAlias(alias) {
+//   alert("======[alias]====:" + alias);
+//   window.JPush.setAlias(
+//     { sequence: 1, alias: alias },
+//     function (result) {
+//       alert("alias设置成功：" + result.alias);
+//     },
+//     function (error) {
+//       alert("err:" + error.code);
+//     }
+//   );
+// }
 serviceWorker.unregister();
